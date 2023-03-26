5015f023729980963587e43e4202bdf9f1d43bd2
Aspartame-s <327127960@qq.com>
2023-03-26 17:54:01 +0800

    添加自定义hook函数

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -13,7 +13,8 @@
 </template>
 
 <script lang='ts'>
-import { defineComponent, onMounted, onUnmounted, ref } from "vue";
+import { defineComponent, watch, ref, Ref } from "vue";
+import useClickOutside from "../hooks/useClickOutside";
 export default defineComponent({
   name: "",
   components: {},
@@ -28,23 +29,31 @@ export default defineComponent({
     const dropdownRef = ref<null | HTMLElement>(null);
     const toggleChange = () => {
       isOpen.value = !isOpen.value;
-      console.log(123)
+      console.log(123);
     };
-    const handler = (e: MouseEvent) => {
-      if (dropdownRef.value) {
-        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen) {
+    const isClickOutside: Ref<boolean> = useClickOutside(dropdownRef);
+    watch(isClickOutside, () => {
+      if (isClickOutside.value) {
+        if (isClickOutside.value && isOpen) {
           isOpen.value = false;
         }
       }
+    });
 
-      
-    };
-    onMounted(() => {
-      document.addEventListener("click", handler);
-    });
-    onUnmounted(() => {
-      document.removeEventListener("click", handler);
-    });
+    // const handler = (e: MouseEvent) => {
+    //   if (dropdownRef.value) {
+    //     if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen) {
+    //       isOpen.value = false;
+    //     }
+    //   }
+
+    // };
+    // onMounted(() => {
+    //   document.addEventListener("click", handler);
+    // });
+    // onUnmounted(() => {
+    //   document.removeEventListener("click", handler);
+    // });
     return {
       isOpen,
       toggleChange,
