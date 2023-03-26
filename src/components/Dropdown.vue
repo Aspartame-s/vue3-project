b82d7123c6ad98e230b1a6138b15c58b46256c38
Aspartame-s <327127960@qq.com>
2023-03-26 16:28:38 +0800

    组件外点击关闭dropdown

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,34 +1,56 @@
 <template>
-  <a class="btn btn-outline-light my-2 dropdown-toggle" @click="toggleChange">
-    {{ title }}
-</a>
-  <ul class="dropdown-menu" :style="{display: 'block'}" v-if="isOpen">
-    <slot></slot>
-  </ul>
+  <div class="dropdown" ref="dropdownRef">
+    <a
+      class="btn btn-outline-light my-2 dropdown-toggle"
+      @click.prevent="toggleChange"
+    >
+      {{ title }}
+    </a>
+    <ul class="dropdown-menu" :style="{ display: 'block' }" v-if="isOpen">
+      <slot></slot>
+    </ul>
+  </div>
 </template>
 
 <script lang='ts'>
-import { defineComponent, ref } from "vue";
+import { defineComponent, onMounted, onUnmounted, ref } from "vue";
 export default defineComponent({
   name: "",
-  components: {
-  },
+  components: {},
   props: {
     title: {
-        type: String,
-        required: true
-    }
+      type: String,
+      required: true,
+    },
   },
   setup() {
-    const isOpen = ref(false)
+    const isOpen = ref(false);
+    const dropdownRef = ref<null | HTMLElement>(null);
     const toggleChange = () => {
-        isOpen.value = !isOpen.value
-    }
+      isOpen.value = !isOpen.value;
+      console.log(123)
+    };
+    const handler = (e: MouseEvent) => {
+      if (dropdownRef.value) {
+        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen) {
+          isOpen.value = false;
+        }
+      }
+
+      
+    };
+    onMounted(() => {
+      document.addEventListener("click", handler);
+    });
+    onUnmounted(() => {
+      document.removeEventListener("click", handler);
+    });
     return {
-        isOpen,
-        toggleChange
-    }
-  }
+      isOpen,
+      toggleChange,
+      dropdownRef,
+    };
+  },
 });
 </script>
 <style scoped>
