911628f4017dd0581c28faa7848750f10c03c11e
chengjiahui <327127960@qq.com>
2023-04-28 17:59:00 +0800

    引入mockjs

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
+if (process.env.NODE_ENV === 'development') {
+    require ('./mock.ts')
+}
+axios.get('/mock/news').then(res => { // url即在mock.js中定义的
+    console.log(res.data) // 打印一下响应数据
+})
+// axios.get('http://apis.imooc.com/api/columns?icode=123').then(res => {
+//     console.log(res)
+// })
 
 
 const app = createApp(App)
